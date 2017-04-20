5f97d5bdf855e66ab1a3243c592b82ce2dc68d8e
Jejomar <jejomardorongon@gmail.com>
2017-04-20 16:23:41 +0800

    Completed part 2 of sprites.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
 	mode: {
 		css: {
+			sprite: 'sprite.svg', //remove .css from the filename
 			render: {
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -18,3 +20,18 @@ gulp.task('createSprite', function() {
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+//copy sprite into the assets/images folder
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+//createSprite in square brackets is dependency, meaning copySprite won't run until createSprite is finished 
+gulp.task('copySpriteCSS', ['createSprite'], function() { 
+	return gulp.src('./app/temp/sprite/css/*.css')
+		.pipe(rename('_sprite.css'))
+		.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
