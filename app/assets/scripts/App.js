ed7f1a99c92a92410b39d014a5d3ed036b4ee575
Jejomar <jejomardorongon@gmail.com>
2017-05-03 05:41:33 +0800

    Just debugged cssInject and webpack automation

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,11 @@
+var $ = require('jquery');
 var Person = require('./modules/Person');
 
+alert("Abc aa21");
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -5,3 +5,5 @@ function Person(fullName, favColor) {
 		console.log("Hello my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
 	}
 }
+
+module.exports = Person;
