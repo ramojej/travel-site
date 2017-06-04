154c6f0acecefdcfc19f33e1f2c9c3da5ef23d79
Jejomar <jejomardorongon@gmail.com>
2017-06-04 20:44:49 +0800

    Added browser support fallbacks.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
 rename = require('gulp-rename'),
-del = require('del');
+del = require('del'),
+svg2png = require('gulp-svg2png');
 
 var config = {
+	shape: {
+		spacing: {
+			padding: 1
+		}
+	},
 	mode: {
 		css: {
+			variables: {
+				replaceSvgWithPng: function() {
+					return function(sprite, render) {
+						return render(sprite).split('.svg').join('.png');
+					}
+				}
+			},
 			sprite: 'sprite.svg', //remove .css from the filename
 			render: {
 				css: {
@@ -26,9 +39,15 @@ gulp.task('createSprite', ['beginClean'], function() {
 		.pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+gulp.task('createPngCopy', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.svg')
+		.pipe(svg2png())
+		.pipe(gulp.dest('./app/temp/sprite/css')); //tell the file where to live
+});
+
 //copy sprite into the assets/images folder
-gulp.task('copySpriteGraphic', ['createSprite'], function() {
-	return gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('copySpriteGraphic', ['createPngCopy'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
 		.pipe(gulp.dest('./app/assets/images/sprites')); //create sprite folder
 });
 
@@ -43,4 +62,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
 	return del('./app/temp/sprite');
 });
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
